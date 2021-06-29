c729dd34937cf65f3e6a189b9f44bf81f784d1f4
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2021-06-29 12:58:37 -0700

    grid responsiveness

--- src/App.js
+++ src/App.js
@@ -10,27 +10,34 @@ function App() {
   const [data, setData] = useState([])
   const [genre, setGenre] = useState('')
   const [clicked, setClicked] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
   const genres = [
     "Action", "Adventure", "Cars", "Comedy", "Dementia", "Demons", "Mystery", "Drama", "Ecchi", "Fantasy", "Game", "Hentai", "Historical",
     "Horror", "Kids", "Magic", "Martial Arts", "Mecha", "Music", "Parody", "Samurai", "Romance", "School", "Sci Fi", "Shoujo", "Shoujo Ai", "Shounen", "Shounen Ai", "Space", "Sports", "Super Power", "Vampire", "Yaoi", "Yuri", "Harem", "Slice of Life", "Supernatural", "Military", "Police", "Psychological", "Thriller", "Seinen", "Josei"
   ]
 
   useEffect(() => {
-    if (genre) {
-      axios.get(`https://api.jikan.moe/v3/search/anime?genre=${genre}&limit=20`)
-      .then(res => {
-        let items = res.data.results
-        setData(items)
-      })
-    }
-    if (search.length > 2) {
-      axios.get(`https://api.jikan.moe/v3/search/anime?q=${search}&limit=20`)
-      .then(res => {
-        let items = res.data.results
-        setData(items)
+    const fetchData = async () => {
+      if (genre) {
+        await axios.get(`https://api.jikan.moe/v3/search/anime?genre=${genre}&limit=30`)
+        .then(res => {
+          let items = res.data.results
+          setData(items)
+          setIsLoading(false)
+        })
+      }
+      if (search.length > 2) {
         setGenre('')
-      })
+        await axios.get(`https://api.jikan.moe/v3/search/anime?q=${search}&limit=30`)
+        .then(res => {
+          let items = res.data.results
+          setData(items)
+          setIsLoading(false)
+        })
+      }
     }
+    fetchData()
+    
   },[search, genre])
 
   const handleSearch = (input) => {
@@ -49,7 +56,7 @@ function App() {
   return (
     <div className="container">
       <Navbar handleClick={handleClick} handleSearch={handleSearch} />
-      <AnimeGrid data={data} />
+      <AnimeGrid data={data} isLoading={isLoading} />
       <Modal clicked={clicked} handleGenreClick={handleGenreClick} genres={genres} />
     </div>
   );
--- src/components/animeGrid/AnimeGrid.js
+++ src/components/animeGrid/AnimeGrid.js
@@ -1,13 +1,18 @@
 import './animeGrid.css'
 import Anime from '../anime/Anime'
 
-const AnimeGrid = ({data}) => {
+const AnimeGrid = ({data, isLoading}) => {
     return (
-        <section className="anime-container">
+        <>
+        {isLoading ? <h2 className="loading">Loading...</h2> :
+            <section className="anime-container">
             {data.map(item => {
                 return <Anime key={item.mal_id} image={item.image_url} title={item.title} />
             })}
         </section>
+        }
+        
+        </>
     )
 }
 
