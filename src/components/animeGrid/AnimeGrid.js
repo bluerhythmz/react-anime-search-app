f2fb4defc1ad1f85d931538ece85f87d6cd35246
allen hunt <bluerhythmz91@gmail.com>
2021-12-17 16:35:11 -0800

    working on anime carousel

--- src/components/animeGrid/AnimeGrid.js
+++ src/components/animeGrid/AnimeGrid.js
@@ -1,8 +1,36 @@
 import styles from "./animeGrid.module.css";
 import Anime from "../anime/Anime";
 import { Link } from "react-router-dom";
+import { useState, useEFfect, useCallback } from "react";
+import { FaAngleLeft } from "react-icons/fa";
+import { FaAngleRight } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import windowDimensions from "../../helpers/UseWindowDimensions";
 
 const AnimeGrid = ({ data, heading, description }) => {
+  const [width, setWidth] = useState(0)
+  const [count, setCount] = useState(0)
+  const [transition, setTransition] = useState("")
+
+  const imageWidthRef = useCallback(
+    (node) => {
+      if (node !== null) {
+        setWidth(node.getBoundingClientRect().width);
+      }
+    },
+    [windowDimensions()]
+  );
+
+  const handleNextClick = () => {
+    setTransition("transform .4s ease-in-out");
+    setCount((prev) => prev + 1);
+  };
+
+  const handlePrevClick = () => {
+    setTransition("transform .4s ease-in-out");
+    setCount((prev) => prev - 1);
+  };
+
   return (
     <section className={styles["grid-wrapper"]}>
       <div className={styles["anime__heading"]}>
@@ -10,10 +38,12 @@ const AnimeGrid = ({ data, heading, description }) => {
         <p className={styles["anime__heading-description"]}>{description}</p>
         <div className={styles["divider"]}></div>
       </div>
-      <div className={styles["anime-container"]}>
-        {data.map((anime) => {
+      <div className={styles["anime-container"]} ref={imageWidthRef}>
+          <div className={styles["anime-links"]} >
+          {data.map((anime) => {
           return (
-            <Link key={anime.mal_id} to={`/anime/${anime.mal_id}/${anime.title}`} style={{textDecoration: "none"}}>
+            <Link key={anime.mal_id} to={`/anime/${anime.mal_id}/${anime.title}`} style={{textDecoration: "none", transform: `translateX(${-width * count}px)`,
+            transition: `${transition}`,}} >
             <Anime
               image={anime.image_url}
               title={anime.title}
@@ -21,7 +51,27 @@ const AnimeGrid = ({ data, heading, description }) => {
             </Link>
           );
         })}
+          </div>
+        
       </div>
+      <IconContext.Provider
+        value={{ className: "", color: "white", size: "3em" }}
+      >
+        <button
+          className={styles["prev"]}
+          onClick={handlePrevClick}
+          
+        >
+          <FaAngleLeft />
+        </button>
+        <button
+          className={styles["next"]}
+          onClick={handleNextClick}
+          
+        >
+          <FaAngleRight />
+        </button>
+      </IconContext.Provider>
     </section>
   );
 };
